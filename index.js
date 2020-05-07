35781095e291efc1b9fe71b491669883fc908c7f
hmn53 <hatim.nalawala987@gmail.com>
2020-05-07 17:04:09 +0530

    mongoose setup

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+//Middlewares
 const app = express();
 require("dotenv").config();
+
+//DB Connect
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) console.error(err);
+    else console.log("Connected with DB");
+  }
+);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server started at port ${process.env.PORT}`)
